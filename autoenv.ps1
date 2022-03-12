41fb98bfdec44bce1fb66a5eebd2cbfd894bbebc
dejvidq <15433565+dejvidq@users.noreply.github.com>
2022-03-12 12:48:37 +0100

    Renamed function from add-autoenv to New-Autoenv, created new function Add-Autoenv for adding existing env for given location. File formatted.

--- autoenv.ps1
+++ autoenv.ps1
@@ -2,12 +2,12 @@ $ConfigPath = Join-Path -Path $HOME -ChildPath "autoenv"
 $ConfigFile = Join-Path -Path $ConfigPath -ChildPath "autoenv.json"
 
 function ConfigSetup() {
-    if (-not (Test-Path -Path $ConfigPath)) {
-	    New-Item $ConfigPath -ItemType directory | Out-Null
-    }
-    if (-not (Test-Path -Path $ConfigFile)) {
-	    New-Item $ConfigFile -ItemType file | Out-Null
-    }
+	if (-not (Test-Path -Path $ConfigPath)) {
+		New-Item $ConfigPath -ItemType directory | Out-Null
+	}
+	if (-not (Test-Path -Path $ConfigFile)) {
+		New-Item $ConfigFile -ItemType file | Out-Null
+	}
 }
 
 function ReadConfig() {
@@ -16,57 +16,93 @@ function ReadConfig() {
 }
 
 function WriteConfig() {
-	param([hashtable]$confObj)
-    $confObj | ConvertTo-Json | Out-File $ConfigFile
+	param([hashtable]$ConfigObject)
+	$ConfigObject | ConvertTo-Json | Out-File $ConfigFile
 }
 
 function smartVenvActivate() {
-    $VirtualEnvs = ReadConfig
+	$VirtualEnvs = ReadConfig
 	$venvActivate = $false
 	if ($VirtualEnvs.Count -gt 0) {
-	    foreach ($el in $VirtualEnvs.GetEnumerator()) {
+		foreach ($el in $VirtualEnvs.GetEnumerator()) {
 			if (($pwd.Path).StartsWith($el.Key)) {
 				$venvActivate = $true
 				$currentEnv = $env:VIRTUAL_ENV
 				if (!("$currentEnv" -eq "$(Join-Path -Path $el.Key -ChildPath $el.Value)")) {
-					# $absolutePath = Resolve-Path -Path $(Join-Path -Path $el.Key -ChildPath $el.Value)
 					$absolutePath = Resolve-Path -Path $(Join-Path -Path $ConfigPath -ChildPath $el.Value)
 					$pathToActivate = Join-Path -Path $absolutePath -ChildPath "Scripts" -AdditionalChildPath "activate.ps1"
-    				& "$pathToActivate"
+					& "$pathToActivate"
 				}
 			}
-	    }
+		}
 	}
 	if ((-not $venvActivate) -and (($env:VIRTUAL_ENV).Length -gt 0)) {
 		deactivate
 	}
 }
 
-function add-autoenv(){
-    param(
-    [string]$Name,
-    [string]$Python="python",
-    [string]$Location=$PWD
-    )
+function New-Autoenv() {
+	param(
+		[Parameter(Mandatory = $true)]
+		[string]$Name,
+		[string]$Python = "python",
+		[string]$Location = $PWD
+	)
 	$Python = $(Get-Command $Python)
 
-    if (Test-Path -Path $Location) {
-        $Location = Resolve-Path $Location
-    } else {
-        Write-Host "WARNING! Path: '$Location' does not exist. Using current path instead!"
-        $Location = $PWD
-    }
-	# Remove trailing / if is passed in Location
-	if (@("/", "\").Contains($Location.Substring($Location.get_Length()-1))) {
-		$Location = $Location.Substring(0, $Location.get_Length()-1)
+	if (Test-Path -Path $Location) {
+		$Location = Resolve-Path $Location
 	}
-    $CurrentConfig = ReadConfig
+ else {
+		Write-Host "WARNING! Path: '$Location' does not exist. Using current path instead!"
+		$Location = $PWD
+	}
+	# Remove trailing / if is passed in Location
+	if (@("/", "\").Contains($Location.Substring($Location.get_Length() - 1))) {
+		$Location = $Location.Substring(0, $Location.get_Length() - 1)
+	}
+	$CurrentConfig = ReadConfig
 	& $Python -m venv $(Join-Path -Path $ConfigPath -ChildPath $Name)
-	$CurrentConfig[$Location] = $Name
-	WriteConfig -confObj $CurrentConfig
+	$CurrentConfig += @{
+		$Location = $Name
+	}
+	WriteConfig -ConfigObject $CurrentConfig
 }
 
-function Set-Location(){
+function Add-Autoenv() {
+	param(
+		[Parameter(Mandatory = $true)]
+		[string]$Name,
+		[string]$Location = $PWD
+	)
+
+	$CurrentConfig = ReadConfig
+	if (Test-Path -Path $(Join-Path -Path $ConfigPath -ChildPath $Name)) {
+		$CurrentConfig[$Location] = $Name
+	} else {
+		Write-Host "There isn't virtualenv with name: '$Name'!"
+	}
+	WriteConfig -ConfigObject $CurrentConfig
+}
+
+function Read-AutoenvConfig() {
+	$CurrentConfig = ReadConfig
+	if ($CurrentConfig.Length -gt 0) {
+		foreach ($venv in $CurrentConfig.GetEnumerator()) {
+			Write-Host "$($venv.Value) -> $($venv.Key)"
+		}
+	} else {
+		Write-Host "There are no virtualenvs created using autoenv"
+	}
+}
+
+function Set-Location() {
 	Microsoft.PowerShell.Management\Set-Location "$Args";
 	smartVenvActivate;
 }
+
+Set-Alias -Name addenv -Value Add-Autoenv
+Set-Alias -Name newenv -Value New-Autoenv
+
+# Run function for config setup on profile loading
+ConfigSetup
